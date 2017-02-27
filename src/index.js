f98d74ab5f962ef3ec407ff74cd426fdc9ab7f5b
archcorsair <archcorsair@gmail.com>
2017-02-27 09:28:25 -0800

    :new: Add main component

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// @flow
+
+import React from 'react'
+
+class ScriptLoader extends React.Component {
+  propTypes = {
+    src: React.PropTypes.string.isRequired,
+    onLoad: React.PropTypes.func.isRequired,
+  };
+  loadFired: boolean;
+  invokeOnLoad() {
+    if (!this.loadFired) {
+      setTimeout(() => {
+        this.loadFired = true
+        this.props.onLoad()
+      }, 0)
+    }
+  }
+  componentDidMount() {
+    const scriptSrc = this.props.src
+    const scripts = Array.from(document.querySelectorAll('script'))
+    if (scripts.find(script => script.src === scriptSrc)) {
+      this.invokeOnLoad()
+      return
+    }
+    const script = document.createElement('script')
+    script.src = scriptSrc
+    script.onload = () => this.invokeOnLoad()
+    document.body.appendChild(script)
+  }
+  render() {
+    return <span style={{ display: 'none' }} data-purpose="Dummy element created by react-isomorphic-scriptloader" />
+  }
+}
+
+module.exports = ScriptLoader
