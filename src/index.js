7f961184bf7e06075899e2e56dc7acecd60bfe69
Daniel Shneyder <daniel.shneyder@dws.la>
2018-10-09 15:56:50 -0700

    Changes to satisfty new lint rules

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 // @flow
 
 import React from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
 class ScriptLoader extends React.Component {
   static propTypes = {
     src: PropTypes.string.isRequired,
     onLoad: PropTypes.func.isRequired,
-  };
-  loadFired: boolean;
-  invokeOnLoad() {
-    if (!this.loadFired) {
-      setTimeout(() => {
-        this.loadFired = true
-        this.props.onLoad()
-      }, 0)
-    }
   }
+
   componentDidMount() {
-    const scriptSrc = this.props.src
+    const { src: scriptSrc } = this.props
     const scripts = Array.from(document.querySelectorAll('script'))
     if (scripts.find(script => script.src === scriptSrc)) {
       this.invokeOnLoad()
@@ -29,6 +21,17 @@ class ScriptLoader extends React.Component {
     script.onload = () => this.invokeOnLoad()
     document.body.appendChild(script)
   }
+  loadFired: boolean
+  invokeOnLoad() {
+    if (!this.loadFired) {
+      setTimeout(() => {
+        this.loadFired = true
+        const { onLoad } = this.props
+        onLoad()
+      }, 0)
+    }
+  }
+
   render() {
     return <span style={{ display: 'none' }} data-purpose="Dummy element created by react-isomorphic-scriptloader" />
   }
